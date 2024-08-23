52e7fb80baca466fa86fada97ae878fc7f99f5a1
SealSergio <sergiobogunio@gmail.com>
2024-08-23 12:57:07 +0300

    dev_1.3
    Make the first version to upload to the server

---
+++ index2.js
@@ -0,0 +1,21 @@
+
+// Вычисляю высоту header
+
+// var height = document.getElementById(".header").offsetHeight
+
+// height = document.getElementById(".header").offsetHeight
+
+// document.querySelector('.contacts').style['scroll-margin-block-start'] = h;
+
+// const addCSS = css => document.head.appendChild(document.createElement("style")).innerHTML = css;
+
+// addCSS('.contacts { scroll-margin-block-start: 160px; }');
+
+// document.querySelector('.contacts').style['scroll-margin-block-start'] = height;
+
+// height.style = '.contacts { scroll-margin-block-start: {$height} }'
+
+// document.querySelector('.contacts').style{'scroll-margin-block-start: '} = h;
+
+// document.documentElement.style
+//     .setProperty('.', 'pink');
